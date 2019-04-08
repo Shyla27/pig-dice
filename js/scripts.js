ffc3f88277e603b1f18e7c477ef1d6ff9f87d23e
SHYLA <sheilabernicew@gmail.com>
2019-04-08 12:04:34 +0300

    add js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,76 @@
-//business-logic
-function Player(rolledNumbers, Score, total) {
-  var rolledNumbers = rolledNumbers
-  var Score = score;
- var total = total;
-}
-Player.prototype.addScore = function() {
-  var total = 0;
-  for (var i = 0; i < this.rolledNumbers.length; i++) {
-    total += this.rolledNumbers[i];
-  }
-  var Score = total;
-  return this.Score;
-}
-Player.prototype.addScore = function() {
-  this.total += this.score;
-  return this.total;
-}
+var score = 0;
+var scorer = 0;
+var score2 = 0;
+var scorer2 = 0;
+var totals = [];
+var totals2 = [];
+ $(document).ready(function() {
+   $("#buttonRoll").click(function() {
+     var dice = Math.floor(Math.random() * 6 + 1);
+     score += dice;
+     document.getElementById('scoreOne').innerHTML = dice;
+     document.getElementById('noo').innerHTML = score;
+     if (dice == 1) {
+       $(".buttons").hide();
+       $(".buttons2").show();
+       score = 0;
+       document.getElementById('noo').innerHTML = score;
+       totals = [];
+     } else {
+       totals.push(dice);
+     }
+   });
+   $("#holdButton").click(function() {
+     totals.forEach(function(total) {
+       return scorer += total;
+     });
+     document.getElementById('totalscore').innerHTML = scorer;
+       $(".buttons").hide();
+       $(".buttons2").show();
+       totals =[];
+       score = 0;
+       document.getElementById('noo').innerHTML = score;
+       if (scorer >= 100) {
+        $(".gamepanel").hide();
+         $(".buttons").hide();
+          $(".buttons2").hide();
+           $("#new-game").show();
+        document.getElementById('finalresult').innerHTML = "PLAYERONE HAS WON THE GAME" ;
+        $(".final").show();
+      }
+   });
+     $("#buttonRoll2").click(function() {
+       var dice2 = Math.floor(Math.random() * 6 + 1);
+       score2 += dice2;
+       document.getElementById('scoreTwo').innerHTML = dice2;
+       document.getElementById('noo1').innerHTML = score2;
+       if (dice2 == 1) {
+         $(".buttons").show();
+         $(".buttons2").hide();
+         score2 = 0;
+         document.getElementById('noo1').innerHTML = score2;
+       } else {
+         totals2.push(score2);
+       }
+     });
+     $("#holdButton2").click(function() {
+       totals2.forEach(function(total) {
+         return scorer2 += total;
+       });
+       document.getElementById('totalscore2').innerHTML = scorer2;
+       $(".buttons2").hide();
+         $(".buttons").show();
+         totals2 =[];
+         score2 = 0;
+         document.getElementById('noo1').innerHTML = score2;
+         if (scorer2 >= 100) {
+          $(".gamepanel").hide();
+           $(".buttons").hide();
+            $(".buttons2").hide();
+             $("#new-game").show();
+          document.getElementById('finalresult').innerHTML = "PLAYERTWO HAS WON THE GAME" ;
+          $(".final").show();
+        }
+     });
 
-function rollDice() {
-  return Math.floor((Math.random() * 6) + 1);
-}
-//function for changing player
-function changePlayer() {
-  $("#rollBtn1").toggle();
-  $("#holdBtn1").toggle();
-  $("#rollBtn2").toggle();
-  $("#holdBtn2").toggle();
-}
-var rolledNumbers1 = [];
-var rolledNumbers2 = [];
-var player1 = new Player(0, 0, 0);
-var player2 = new Player(0, 0, 0);
-
-//front-end logic
-$(document).ready(function() {
-  $("#startButton").click(function() {
-    location.reload();
-  });
-
-  $("#rollBtn1").click(function(event) {
-    event.preventDefault();
-    var rolledNumber = rollDice();
-    $("#diceNumber").text(rolledNumber);
-    if (rolledNumber != 1) {
-      rolledNumbers1.push(rolledNumber);
-      player1.rolledNumbers = rolledNumbers1;
-      player1.addScore();
-      $("#roundResult1").text(player1.tempScore);
-    } else {
-      player1.rolledNumbers = 0;
-      rolledNumbers1 = [0];
-      player1.addTempScore();
-      $("#roundResult1").text(player1.add);
-      $("#totalResult1").text(player1.total);
-      alert("You rolled 1. please pass the dice");
-      changePlayer();
-    }
-
-
-  });
-
-  $("#holdBtn1").click(function(event) {
-    event.preventDefault();
-    $("#totalResult1").text(player1.addTotal());
-    if (player1.totalScore >= 100) {
-      alert("Player2 is the winner."+ "Your Score is " + player1.totalScore);
-      location.reload();
-    }
-    $("#roundResult1").text(0);
-    rolledNumbers1 = [0];
-    changePlayer();
-  });
-
-  $("#rollBtn2").click(function(event) {
-    event.preventDefault();
-
-    var rolledNumber = rollDice();
-    $("#diceNumber").text(rolledNumber);
-    if (rolledNumber != 1) {
-      rolledNumbers2.push(rolledNumber);
-      player2.rolledNumbers = rolledNumbers2;
-      $("#roundResult2").text(player2.addScore());
-    } else {
-      player2.rolledNumbers = 0;
-      $("#roundResult2").text(player2.addScore());
-      $("#totalResult2").text(player2.addTotal());
-      alert("You rolled 1.please pass the dice ");
-      changePlayer();
-      rolledNumbers2 = [0];
-    }
-  });
-
-  $("#holdBtn2").click(function(event) {
-    $("#totalResult2").text(player2.addTotal());
-    $("#roundResult2").text(0);
-    if (player2.totalScore >= 100) {
-      alert("Player2 is the winner." +"Your score is " + player2.total);
-      location.reload();
-    }
-    rolledNumbers2 = [0];
-    changePlayer();
-  });
-});
+ });
